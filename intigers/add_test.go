19455f9881895eb992195e02c5580bb1c59af32f
Alazar Beyene <alazarbeyene99@gmail.com>
2022-06-23 17:05:22 +0300

    integer testing and the use of example to make it more readable out test

--- intigers/add_test.go
+++ intigers/add_test.go
@@ -1,11 +1,25 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+func ExampleAddr() {
+	sum := Addr(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
 
 func TestAddr(t *testing.T) {
 	expect := 3
 	sum := Addr(1, 2)
 	if expect != sum {
 		t.Errorf("%d what and get %d", expect, sum)
+	}
+	expect2 := 3
+	sum2 := Addr(1, 2)
+	if expect2 != sum2 {
+		t.Errorf("%d what and get %d", expect2, sum2)
 	}
 }
